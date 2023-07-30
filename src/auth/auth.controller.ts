41a6992241a2ad7d6c594a04cca22d6b5c957d5a
Jaranchai Nut <jaranchai.nt@hotmail.com>
2023-07-30 18:07:09 +0700

    basic api with prisma

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  // authService: AuthService;
+  // constructor(authService: AuthService) {
+  //   this.authService = authService;
+  // }
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
+}
