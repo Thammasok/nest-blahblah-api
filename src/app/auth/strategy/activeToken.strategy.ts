1138f90d09bb40b07995d73ee6841ca2a84f5665
Jaranchai Nut <jaranchai.nt@hotmail.com>
2023-09-09 12:10:00 +0700

    update code

---
+++ src/app/auth/strategy/activeToken.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { JwtPayload } from '../types'
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false, // default
+      secretOrKey: configService.get<string>('auth.access_token_secret'),
+    })
+  }
+
+  validate(payload: JwtPayload) {
+    return payload
+    //payload는 jwt token decode 한 것
+  } // req.user에 저장
+}
--- src/app/auth/strategy/index.ts
+++ src/app/auth/strategy/index.ts
@@ -1,2 +1,4 @@
+export * from './activeToken.strategy'
+export * from './refreshToken.strategy'
 export * from './jwt.strategy'
 export * from './uuid.strategy'
